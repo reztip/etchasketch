f3a219d73dbdfcd6eb6ca90b76fa8282a0ac0ea9
Alex P <alexpitzer42@gmail.com>
2015-06-18 20:51:06 -0700

    adding a fadein for grid resets, need to properly implement grid resets

--- js/jquery.js
+++ js/jquery.js
@@ -1,31 +1,37 @@
-var xCols = 0;
+/*var xCols = 0;
 var yCols = 0;
-var count = 0;
+var gridWidth =  600; //in px
+var gridHeight = 600; //in px
+var count = 0;*/
 $(document).ready( function() {
 
+  var resetGrid = function(rows, cols){
+  	$('.gridcontainer').fadeOut('fast');
+  	$('.gridcontainer').empty();
+  	$('.gridcontainer').fadeIn('slow');
+  };
+
+  resetGrid(16,16);
   $('.gridcontainer').click(function() {
 
   	$(this).add("<div class='square'>ok</div>");
-  	alert('square added');
+  
 
   });
 
-  $('button').click(function() {
-    xCols = showSizePrompt("columns");
- 	yCols = showSizePrompt('rows');
+  $('.gridcontainer').on('hover', '.square', function(){
+
+  	$(this).css('background-color', 'black');
+  	$(this).fadeIn();
   });
 
 
+  $('button').on('click', function(){
+  	var rows = +prompt("how many rows do you want?");
+  	var cols = +prompt("how many columns do you want?");
+  	resetGrid(rows,cols);
+  });
+
+  
 
 });
-
-var isNumeric = function() {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
-
-
-var showSizePrompt(messg){
-	var y = prompt("How many "+ mssg + "do you want"?);
-	while !isNumeric(y) y = prompt("Re enter a positive number");
-	return y;
-}
